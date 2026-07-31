bdd149b960dc75ed7cf3d185f790aba9a98ad258
Rutanshushah <shahrutanshu@gmail.com>
2026-07-31 17:37:22 +0530

    Update factorial of the number .cpp

--- factorial of the number .cpp
+++ factorial of the number .cpp
@@ -1,1 +1,11 @@
-
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    long long fact=1;
+    cout<<"ENTER A NUMBER :";
+    cin>>n;
+    for(int i=1; i<=n;i++)fact*=i;
+    cout<<"factorial="<<fact;
+    return 0;
+}
