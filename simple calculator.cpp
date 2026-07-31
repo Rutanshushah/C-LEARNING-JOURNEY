f208efd2e7b267fc992680e3fd03dd4377bc1af0
Rutanshushah <shahrutanshu@gmail.com>
2026-07-31 14:44:58 +0530

    Update simple calculator.cpp

--- simple calculator.cpp
+++ simple calculator.cpp
@@ -1,1 +1,30 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    char op;
 
+    cout  <<" ENTER FRIST NUMBER:";
+    cin >> a;
+    cout<<"ENTER OPERATOR (+,-,*,/):";
+    cin>>op;
+    cout<<"ENTER SECOND NUMBER:";
+    cin>>b;
+
+    switch (op){
+        case'+': cout<<"result="<<a+b;
+        break;
+        case'-': cout<<"result="<<a-b;
+        break;
+        case'*': cout<<"result="<<a*b;
+        break;
+        case'/':  if (b!=0)
+        cout<<"result="<<(float)a/b;
+        else
+        cout<<"cannot divide by zero!";
+        break;
+        befault:
+        cout<<"invalid operator!";
+    }
+   return 0;
+}
