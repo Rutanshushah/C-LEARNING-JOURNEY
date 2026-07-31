d659d4070216fcf6b0647b2ba9eeb421fb10b8f1
Rutanshushah <shahrutanshu@gmail.com>
2026-07-31 17:30:24 +0530

    Update even and odd number.cpp

--- even and odd number.cpp
+++ even and odd number.cpp
@@ -1,1 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num;
+    cout<<"ENTER A NUMBER:";
+    cin>>num;
+    if(num%2==0)
+        cout<<num<<"IS EVEN.";
+    else
+    cout<<num<<"IS ODD.";
+return 0;
+}
+
 
