82aa01284d053eee0d2175786e37b15da5696d6a
Rutanshushah <shahrutanshu@gmail.com>
2026-07-31 17:47:23 +0530

    Update multiplication table.cpp

--- multiplication table.cpp
+++ multiplication table.cpp
@@ -1,1 +1,12 @@
-
+#include<iostream>
+using namespace std;
+int main(){
+    int num;
+    cout<<"ENTER A NUMBER:";
+    cin>>num;
+    for(int i=1; i<=10;i++)
+{
+    cout<<num<<"x"<<i<<"="<<num*i<<endl;
+}
+return 0;
+}
