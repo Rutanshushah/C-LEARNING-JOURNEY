334778dcff84747bba5a1b439736a4dee679c36a
Rutanshushah <shahrutanshu@gmail.com>
2026-07-31 11:35:32 +0530

    PROJECT-1.cpp balance decerasing

--- PROJECT-1.cpp
+++ PROJECT-1.cpp
@@ -1,1 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    double balance;
 
+    cout<<"Enter your current balance: ";
+    cin>>balance;
+
+    while(balance>0){
+        balance=balance-100;
+        cout<<"Your balance is decreasing,\n"<<balance<<"remaining balance!\n";
+    }
+    cout<<"your balance balance is empty!";
+
+}
