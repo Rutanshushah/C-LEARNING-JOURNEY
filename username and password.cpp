d8b4a0861eab6333ac660d83def4cdae08cfd528
Rutanshushah <shahrutanshu@gmail.com>
2026-08-02 12:13:22 +0530

    Update username and password.cpp

--- username and password.cpp
+++ username and password.cpp
@@ -1,1 +1,21 @@
+#include<stdio.h>
+int main (){
+    int username , password;
+    //correct username and password
+    int correctUsername=1111;
+    int correctPassword=7777;
 
+    //input
+    printf("ENTER USERNAME:");
+    scanf("%d",&username);
+    printf("ENTER PASSWORD:");
+    scanf("%d",&password);
+    //check credentials
+    if (username==correctUsername && password==correctPassword) {
+        printf("Login sucessfully,You Are Entered Successfully.");
+    } else {
+        printf("invalid credentials.");
+    }
+
+    return 0;
+}
